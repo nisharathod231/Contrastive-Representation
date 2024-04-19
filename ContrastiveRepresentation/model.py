6adeac69cf6babd9e80d3814c0e8cb9b1e3ab0cd
nisharathod231 <nisharathod27899@gmail.com>
2024-04-19 13:06:53 +0530

    Flatten all dimensions except the batch dimension

--- ContrastiveRepresentation/model.py
+++ ContrastiveRepresentation/model.py
@@ -2,31 +2,40 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class Encoder(nn.Module):
-    def __init__(self, output_dim=128):
+    def __init__(self, d_out=128):
         super(Encoder, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-        self.fc = nn.Linear(128 * 4 * 4, output_dim)
+        # First convolutional block
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        # Second convolutional block
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+        
+        # Fully connected layers
+        self.fc1 = nn.Linear(64 * 8 * 8, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        
+        # Output layer
+        self.out = nn.Linear(512, d_out)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = self.pool(x)
         x = F.relu(self.conv2(x))
         x = self.pool(x)
-        x = F.relu(self.conv3(x))
-        x = self.pool(x)
-        x = x.view(-1, 128 * 4 * 4)
-        x = self.fc(x)
+        x = torch.flatten(x, 1)  # flatten all dimensions except the batch dimension
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.out(x)
         return x
-    
+
 class Classifier(nn.Module):
-    def __init__(self, input_dim, num_classes=10):
+    def __init__(self, d_in, num_classes):
         super(Classifier, self).__init__()
-        self.fc = nn.Linear(input_dim, num_classes)
-        
+        self.fc = nn.Linear(d_in, num_classes)
+
     def forward(self, x):
         x = self.fc(x)
         return x
