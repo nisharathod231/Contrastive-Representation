00849b3823dfb2f0536549b7550adb1e869d5522
nisharathod231 <nisharathod27899@gmail.com>
2024-04-19 12:56:50 +0530

    Add NLLL before encoding

--- ContrastiveRepresentation/main.py
+++ ContrastiveRepresentation/main.py
@@ -1,73 +1,140 @@
 import torch
 from argparse import Namespace
-import numpy as np
-from torch.utils.data import DataLoader, TensorDataset
+import os
 
-# Assuming ContrastiveRepresentation.pytorch_utils imports correctly
 import ContrastiveRepresentation.pytorch_utils as ptu
-from utils import get_data, train_test_split, plot_tsne, plot_losses, plot_accuracies
+from utils import *
+from LogisticRegression.model import SoftmaxRegression as LinearClassifier
 from ContrastiveRepresentation.model import Encoder, Classifier
 from ContrastiveRepresentation.train_utils import fit_contrastive_model, fit_model
-from LogisticRegression.model import SoftmaxRegression  # Assuming this is your linear classifier
+
+def save_model(model, filename):
+    np.savez(filename, W=model.W, b=model.b)
+
+def save_model_pytorch(model, filepath):
+    torch.save(model.state_dict(), filepath)
+
+
+
 
 def main(args: Namespace):
+    '''
+    Main function to train and generate predictions in csv format
+
+    Args:
+    - args : Namespace : command line arguments
+    '''
     torch.manual_seed(args.sr_no)
-    X, y = get_data(args.train_data_path, is_linear=False)
+
+    # Get the training data
+    X, y = get_data(args.train_data_path)
     X_train, y_train, X_val, y_val = train_test_split(X, y)
     
-    # Convert to PyTorch tensors
-    X_train, y_train, X_val, y_val = map(lambda x: ptu.from_numpy(x), (X_train, y_train, X_val, y_val))
+    print("Converting to torch tensors...")
+    # TODO: Convert the images and labels to torch tensors using pytorch utils (ptu)
+    X_train = ptu.from_numpy(X_train).float().to(ptu.device)
+    y_train = ptu.from_numpy(y_train).long().to(ptu.device)
+    X_val = ptu.from_numpy(X_val).float().to(ptu.device)
+    y_val = ptu.from_numpy(y_val).long().to(ptu.device)    
 
+    print("Converted the images and labels to torch tensors using pytorch utils (ptu)")
+
+    # Create the model
     encoder = Encoder(args.z_dim).to(ptu.device)
-    classifier = None  # Default to None
-
     if args.mode == 'fine_tune_linear':
-        classifier = SoftmaxRegression(input_dim=args.z_dim, output_dim=10).to(ptu.device)
-    elif args.mode == 'fine_tune_nn':
-        classifier = Classifier(input_dim=args.z_dim, num_classes=10).to(ptu.device)
-    
-    if args.mode == 'cont_rep':
-        # Assume ContrastiveDataset is a custom Dataset class you've implemented
-        train_dataset = ContrastiveDataset(X_train, y_train)
-        train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
-        fit_contrastive_model(encoder, train_loader, args)
+        # classifier = # TODO: Create the linear classifier model
+        classifier = LinearClassifier(inp_dim=args.z_dim)
         
-        # Assume implementation for plot_tsne exists
-        # plot_tsne(encoder, X_val, y_val)  # You may need to adjust this call based on actual implementation
+    elif args.mode == 'fine_tune_nn':
+        # classifier = # TODO: Create the neural network classifier model
+        classifier = Classifier(d_in=args.z_dim, num_classes=len(set(y_train.tolist()))).to(ptu.device)
+        classifier = classifier.to(ptu.device)
 
-        torch.save(encoder.state_dict(), args.encoder_path)
-    else:
-        if args.load_encoder:
-            encoder.load_state_dict(torch.load(args.encoder_path))
+    if args.mode == 'cont_rep':
+        # Fit the contrastive model
+        print("Fitting CR...")
+        losses = fit_contrastive_model(encoder, X_train, y_train, args.num_iters, args.batch_size, args.lr)
+        print("Fitting CR is completed!")
+        # Obtain the embeddings for plotting
 
-        # Here you should convert your data into a DataLoader
-        train_loader = DataLoader(TensorDataset(X_train, y_train), batch_size=args.batch_size, shuffle=True)
-        val_loader = DataLoader(TensorDataset(X_val, y_val), batch_size=args.batch_size)
-        fit_model(encoder, classifier, train_loader, val_loader, args)
+        print("saving encoder...")
+        # Ensure that the directory exists
+        os.makedirs(os.path.dirname(args.encoder_path), exist_ok=True)
 
-        # For test predictions
+        # Save the encoder's state dictionary
+        torch.save(encoder.state_dict(), './models/encoder.pth')
+
+        #torch.save(encoder.state_dict(), f'{args.encoder_path}')
+        print(f"Encoder is saved to models/encoder.pth.")
+        print("Plotting losses of CR...")
+        plt.plot(losses)
+        plt.xlabel('Iterations')
+        plt.ylabel('Loss')
+        plt.title('Contrastive Representation Learning Loss')
+        plt.savefig(f'images/2.2.png')
+        print("Plotted! check out images/2.2png")
+
+        
+
+        for i in range(0, len(X_train), args.batch_size):
+            X_batch = X_train[i:i+args.batch_size]
+            X_batch = ptu.to_numpy(encoder(X_batch))
+            if i==0:
+                temp = X_batch
+            else:
+                temp = np.concatenate((temp, X_batch), axis=0)
+         
+        z = temp
+       
+        plot_tsne(z, y_train)
+ 
+ 
+
+        
+    else: # train the classifier (fine-tune the encoder also when using NN classifier)
+        # Load the encoder
+        encoder.load_state_dict(torch.load('./models/encoder.pth'))
+        encoder = Encoder(args.z_dim).to(ptu.device)
+
+        # raise NotImplementedError('Load the encoder')
+        
+        # Fit the model
+        train_losses, train_accs, test_losses, test_accs = fit_model(
+            encoder, classifier, X_train, y_train, X_val, y_val, args)
+        
+        # Plot the losses
+        plot_losses(train_losses, test_losses, f'{args.mode} - Losses')
+        
+        # Plot the accuracies
+        plot_accuracies(train_accs, test_accs, f'{args.mode} - Accuracies')
+        
+        # # Get the test data
         X_test, _ = get_data(args.test_data_path)
         X_test = ptu.from_numpy(X_test).float()
-        test_loader = DataLoader(X_test, batch_size=args.batch_size)
-        save_predictions_to_csv(encoder, classifier, test_loader, args)
 
-def save_predictions_to_csv(encoder, classifier, data_loader, args):
-    encoder.eval()
-    classifier.eval()
-    y_preds = []
-    with torch.no_grad():
-        for X_batch, in data_loader:
-            X_batch = X_batch.to(ptu.device)
-            embeddings = encoder(X_batch)
-            y_pred_batch = classifier(embeddings)
-            y_preds.extend(y_pred_batch.argmax(dim=1).cpu().numpy())
-    np.savetxt(f'data/{args.sr_no}_predictions.csv', y_preds, delimiter=',', fmt='%d')
-    print(f'Predictions saved to data/{args.sr_no}_predictions.csv')
+        # # Save the predictions for the test data in a CSV file
+        y_preds = []
+        for i in range(0, len(X_test), args.batch_size):
+            X_batch = X_test[i:i+args.batch_size].to(ptu.device)
+            repr_batch = encoder(X_batch)
+            if 'linear' in args.mode:
+                repr_batch = ptu.to_numpy(repr_batch)
+            y_pred_batch = classifier(repr_batch)
+            if 'nn' in args.mode:
+                y_pred_batch = ptu.to_numpy(y_pred_batch)
+            y_preds.append(y_pred_batch)
+        y_preds = np.concatenate(y_preds).argmax(axis=1)
 
-if __name__ == "__main__":
-    # Placeholder for argument parsing
-
-# if __name__ == "__main__":
-#     # Example: Parse command-line arguments and call main
-#     args = Namespace(sr_no=12345, train_data_path="path/to/train_data", test_data_path="path/to/test_data", z_dim=128, batch_size=64, epochs=10, lr=1e-3, mode="cont_rep", load_encoder=False)
-#     main(args)
+        print("Saving predictions...")
+        np.savetxt(f'data/{args.sr_no}_{"repr_lin" if "linear" in args.mode else "repr_nn"}.csv',\
+                y_preds, delimiter=',', fmt='%d')
+        print(f'Predictions saved to data/{args.sr_no}_{"repr_lin" if "linear" in args.mode else "repr_nn"}.csv')
+        
+        if args.mode == 'fine_tune_nn':
+            print("Saving classifier...")
+            save_model_pytorch(classifier, 'models/classifier.pth')
+            print(f"Classifier is saved to models/classifier.pth.")
+        else:
+            print("Saving softmaxregression model...")
+            save_model(classifier, 'models/softmax_regression_model.npz')
+            print(f"Classifier is saved to models/softmax_regression_model.npz.")
